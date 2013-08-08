67f6ed0dbdaa532b86585c2544b25c8cf01a7f5b
Chris Broadfoot <chris@chrisbroadfoot.id.au>
2013-08-08 19:01:10 +1000

    Add copyright headers to some files, remove erroneous main function from SphericalMercatorProjection

--- library/src/com/google/maps/android/projection/SphericalMercatorProjection.java
+++ library/src/com/google/maps/android/projection/SphericalMercatorProjection.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.maps.android.projection;
 
 import com.google.android.gms.maps.model.LatLng;
@@ -25,12 +41,5 @@ public class SphericalMercatorProjection {
         final double lat = 90 - Math.toDegrees(Math.atan(Math.exp(-y * 2 * Math.PI)) * 2);
 
         return new LatLng(lat, lng);
-    }
-
-    public static void main(String[] args) {
-        SphericalMercatorProjection sphericalMercatorProjection = new SphericalMercatorProjection(256);
-        Point point = sphericalMercatorProjection.toPoint(new LatLng(41.850033, -87.65005229999997));
-        System.out.println(point);
-        System.out.println(sphericalMercatorProjection.toLatLng(point));
     }
 }
