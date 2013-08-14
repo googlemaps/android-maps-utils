a686cc50ad7e29f9e403762fe33d266161eb0d9a
Chris Broadfoot <chris@chrisbroadfoot.id.au>
2013-08-14 16:50:22 +1000

    DefaultView: use buckets for icon text. Don't show the cluster if there are fewer than 4 points. Only refresh the view if the clusters change.

--- library/src/com/google/maps/android/clustering/view/DefaultView.java
+++ library/src/com/google/maps/android/clustering/view/DefaultView.java
@@ -1,6 +1,8 @@
 package com.google.maps.android.clustering.view;
 
 import android.content.Context;
+import android.util.Log;
+import android.util.SparseArray;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptor;
@@ -19,6 +21,8 @@ public class DefaultView<T extends ClusterItem> implements ClusterView<T> {
     private final GoogleMap mMap;
     private final BubbleIconFactory mBubbleIconFactory;
     private final Collection<Marker> mMarkers = new HashSet<Marker>();
+    private SparseArray<BitmapDescriptor> mIcons = new SparseArray<BitmapDescriptor>();
+    private Set<? extends Cluster<T>> mCurrentClusters;
 
     public DefaultView(Context context, GoogleMap map) {
         mMap = map;
@@ -26,18 +30,21 @@ public class DefaultView<T extends ClusterItem> implements ClusterView<T> {
         mBubbleIconFactory.setStyle(BubbleIconFactory.Style.BLUE);
     }
 
-    private Marker onCreateCluster(Cluster<T> cluster) {
-        if (cluster.getSize() == 1) {
+    private void onCreateCluster(Cluster<T> cluster) {
+        if (cluster.getSize() < 4) {
             for (ClusterItem item : cluster.getItems()) {
-                return mMap.addMarker(item.getMarkerOptions());
+                Marker marker = mMap.addMarker(item.getMarkerOptions());
+                mMarkers.add(marker);
             }
+            return;
         }
 
         BitmapDescriptor icon = getBitmapDescriptor(cluster);
-        return mMap.addMarker(new MarkerOptions().
+        Marker marker = mMap.addMarker(new MarkerOptions().
                 icon(icon).
                 title(getTitle(cluster)).
                 position(cluster.getPosition()));
+        mMarkers.add(marker);
     }
 
     private String getTitle(Cluster cluster) {
@@ -45,17 +52,52 @@ public class DefaultView<T extends ClusterItem> implements ClusterView<T> {
     }
 
     private BitmapDescriptor getBitmapDescriptor(Cluster cluster) {
-        return BitmapDescriptorFactory.fromBitmap(mBubbleIconFactory.makeIcon(cluster.getSize() + ""));
+        return getIcon(getBucket(cluster.getSize()));
+    }
+
+    private BitmapDescriptor getIcon(int bucket) {
+        BitmapDescriptor descriptor = mIcons.get(bucket);
+        if (descriptor == null) {
+            descriptor = BitmapDescriptorFactory.fromBitmap(mBubbleIconFactory.makeIcon(bucket + (bucket >= 10 ? "+" : "")));
+            mIcons.put(bucket, descriptor);
+        }
+        return descriptor;
+    }
+
+    private int getBucket(int size) {
+        if (size < 10) {
+            return size;
+        }
+        if (size < 20) {
+            return 10;
+        }
+        if (size < 40) {
+            return 20;
+        }
+        if (size < 100) {
+            return 40;
+        }
+        if (size < 200) {
+            return 100;
+        }
+        if (size < 500) {
+            return 200;
+        }
+        return 500;
     }
 
     @Override
     public void onClustersChanged(Set<? extends Cluster<T>> clusters) {
+        if (clusters.equals(mCurrentClusters)) {
+            return;
+        }
+        mCurrentClusters = clusters;
         for (Marker m : mMarkers) {
             m.remove();
         }
         mMarkers.clear();
         for (Cluster c : clusters) {
-            mMarkers.add(onCreateCluster(c));
+            onCreateCluster(c);
         }
     }
 }
