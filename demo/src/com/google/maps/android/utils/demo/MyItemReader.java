3f7787b41ae6715e5072d13e0b4f3937f8b62912
Maciej Górski <maciek.gorski@gmail.com>
2013-08-16 10:28:13 +0200

    comment on the Scanner usage to convert InputStream to String

--- demo/src/com/google/maps/android/utils/demo/MyItemReader.java
+++ demo/src/com/google/maps/android/utils/demo/MyItemReader.java
@@ -13,9 +13,16 @@ import com.google.maps.android.utils.demo.model.MyItem;
 
 public class MyItemReader {
 
+    /*
+     * This matches only once in whole input,
+     * so Scanner.next returns whole InputStream as a String.
+     * http://stackoverflow.com/a/5445161/2183804
+     */
+    private static final String REGEX_INPUT_BOUNDARY_BEGINNING = "\\A";
+
     public List<MyItem> read(InputStream inputStream) throws JSONException {
         List<MyItem> items = new ArrayList<MyItem>();
-        String json = new Scanner(inputStream).useDelimiter("\\A").next();
+        String json = new Scanner(inputStream).useDelimiter(REGEX_INPUT_BOUNDARY_BEGINNING).next();
         JSONArray array = new JSONArray(json);
         for (int i = 0; i < array.length(); i++) {
             JSONObject object = array.getJSONObject(i);
