45350457f185e5941123da7374e2c33cb2326643
irisu <i.uy@student.unsw.edu.au>
2013-12-10 16:19:58 +1100

    Wrote getLocation method

--- library/src/com/google/maps/android/quadtree/LinearQuadTree.java
+++ library/src/com/google/maps/android/quadtree/LinearQuadTree.java
@@ -12,17 +12,44 @@ import java.util.Collection;
 public class LinearQuadTree<T extends LinearQuadTree.Item> implements QuadTree<T> {
 
     private class Node {
+        public final int base = 5;
         public int location;
-        public Point point;
+        public T t;
 
-        public Node(Item item) {
+        public Node(T item) {
             location = getLocation(item.getPoint());
-            this.point = item.getPoint();
+            this.t = item;
         }
 
         private int getLocation(Point p) {
-            return 0;
+            int location = 0;
+            Bounds currBounds = mBounds;
+            for (int currPrecision = mPrecision-1; currPrecision >= 0; currPrecision--) {
+                if (p.y < currBounds.midY) {       // top
+                    if (p.x < currBounds.midX) {   // left = 0
+                        currBounds = new Bounds(currBounds.minX, currBounds.midX,
+                                                     currBounds.minY, currBounds.midY);
+                    } else {                    // right = 1
+                        location += 1 * base^currPrecision;
+                        currBounds = new Bounds(currBounds.midX, currBounds.maxX,
+                                                     currBounds.minY, currBounds.midY);
+                    }
+                } else {                        // bottom
+                    if (p.x < currBounds.midX) {   // left = 2
+                        location += 2 * base^currPrecision;
+                        currBounds = new Bounds(currBounds.minX, currBounds.midX,
+                                                     currBounds.midY, currBounds.maxY);
+                    } else {                    // right = 3
+                        location += 3 * base^currPrecision;
+                        currBounds = new Bounds(currBounds.midX, currBounds.maxX,
+                                                     currBounds.midY, currBounds.maxY);
+                    }
+                }
+            }
+            return location;
         }
+
+        // TODO: write compare
 
     }
 
@@ -33,7 +60,7 @@ public class LinearQuadTree<T extends LinearQuadTree.Item> implements QuadTree<T
 
     private ArrayList<Node> mPoints;
 
-    public int precision = 10;
+    public int mPrecision;
 
     /**
      * Creates a new quad tree with specified bounds.
@@ -43,31 +70,37 @@ public class LinearQuadTree<T extends LinearQuadTree.Item> implements QuadTree<T
      * @param minY
      * @param maxY
      */
-    public LinearQuadTree(double minX, double maxX, double minY, double maxY) {
-        this(new Bounds(minX, maxX, minY, maxY));
+    public LinearQuadTree(double minX, double maxX, double minY, double maxY, int precision) {
+        this(new Bounds(minX, maxX, minY, maxY), precision);
     }
 
-    public LinearQuadTree(Bounds bounds) {
+    public LinearQuadTree(Bounds bounds, int precision) {
+        mPoints = new ArrayList<Node>();
         mBounds = bounds;
+        mPrecision = precision;
     }
 
     @Override
     public void add(T item) {
-
+        int index = 0;
+        //TODO: set index
+        mPoints.add(index, new Node(item));
     }
 
     @Override
     public boolean remove(T item) {
+        //TODO
         return false;
     }
 
     @Override
     public void clear() {
-
+        mPoints.clear();
     }
 
     @Override
     public Collection<T> search(Bounds searchBounds) {
+        //TODO
         return null;
     }
 }
