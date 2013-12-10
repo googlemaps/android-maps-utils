d58acccdb5886046d5d7268cc6b864f0474d018a
irisu <i.uy@student.unsw.edu.au>
2013-12-10 15:11:48 +1100

    Set up LinearQuadTree stub

--- library/src/com/google/maps/android/quadtree/LinearQuadTree.java
+++ library/src/com/google/maps/android/quadtree/LinearQuadTree.java
@@ -1,13 +1,37 @@
 package com.google.maps.android.quadtree;
 
 import com.google.maps.android.geometry.Bounds;
+import com.google.maps.android.geometry.Point;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
  * Created by irisu on 12/9/13.
  */
-public class LinearQuadTree<T extends QuadTree.Item> implements QuadTree<T> {
+public class LinearQuadTree<T extends LinearQuadTree.Item> implements QuadTree<T> {
+
+    /**
+     * The bounds of this quad.
+     */
+    private final Bounds mBounds;
+
+    /**
+     * Creates a new quad tree with specified bounds.
+     *
+     * @param minX
+     * @param maxX
+     * @param minY
+     * @param maxY
+     */
+    public LinearQuadTree(double minX, double maxX, double minY, double maxY) {
+        this(new Bounds(minX, maxX, minY, maxY));
+    }
+
+    public LinearQuadTree(Bounds bounds) {
+        mBounds = bounds;
+    }
+
     @Override
     public void add(T item) {
 
