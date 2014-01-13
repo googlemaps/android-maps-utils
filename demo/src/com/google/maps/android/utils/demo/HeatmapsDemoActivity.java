2212974473d4a6126f83bb4243717d2c00cd1c30
microcat <mintymelody@gmail.com>
2014-01-13 11:54:56 +1100

    Spinner for demo.

--- demo/src/com/google/maps/android/utils/demo/HeatmapsDemoActivity.java
+++ demo/src/com/google/maps/android/utils/demo/HeatmapsDemoActivity.java
@@ -1,7 +1,11 @@
 package com.google.maps.android.utils.demo;
 
+import android.app.Activity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,6 +20,7 @@ import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -25,24 +30,21 @@ import java.util.Scanner;
  */
 public class HeatmapsDemoActivity extends BaseDemoActivity {
 
-    private HeatmapHelper mHeatmapHandler;
+    private HeatmapHelper mHeatmapHelper;
 
     private boolean defaultGradient = true;
     private boolean defaultRadius = true;
     private boolean defaultOpacity = true;
     private boolean origData = true;
 
+
     /**
-     * List of LatLngWrappers
+     * Maps name of data set to data (list of LatLngWrappers)
      * Each LatLngWrapper contains a LatLng as well as corresponding intensity value (which
      * represents "importance" of this LatLng) - see the class for more detail
      */
-    private ArrayList<LatLngWrapper> mList;
-
-    /**
-     * Alternative data set
-     */
-    private ArrayList<LatLngWrapper> mListThinned;
+    private HashMap<String, ArrayList<LatLngWrapper>> mLists =
+            new HashMap<String, ArrayList<LatLngWrapper>>();
 
     @Override
     protected int getLayoutId() {
@@ -51,13 +53,20 @@ public class HeatmapsDemoActivity extends BaseDemoActivity {
 
     @Override
     protected void startDemo() {
-        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(34.0522300, -118.2436800), 7));
+        // getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-37.8140000, 144.9633200), 5));
+        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-25, 135), 3 ));
 
-        mList = new ArrayList<LatLngWrapper>();
-        mListThinned = new ArrayList<LatLngWrapper>();
+        // Set up the spinner/dropdown list
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.heatmaps_datasets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new SpinnerActivity());
 
         try {
-            readItems();
+            mLists.put(getString(R.string.police_stations), readItems(R.raw.policeall));
+            mLists.put(getString(R.string.red_lights), readItems(R.raw.redlights));
         } catch (JSONException e) {
             Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show();
         }
@@ -67,7 +76,8 @@ public class HeatmapsDemoActivity extends BaseDemoActivity {
         // intensity from, and the map to draw the heatmap on
         // radius, gradient and opacity not specified, so default are used
         try {
-            mHeatmapHandler = new HeatmapHelper.Builder(mList, getMap()).zoom(5, 8).build();
+            mHeatmapHelper = new HeatmapHelper.Builder(
+                    mLists.get(getString(R.string.police_stations)), getMap()).build();
         } catch(IllegalArgumentException e) {
             Log.e("IllegalArgumentException in Builder", e.getMessage());
         }
@@ -75,63 +85,65 @@ public class HeatmapsDemoActivity extends BaseDemoActivity {
 
     public void changeRadius(View view) {
         if (defaultRadius) {
-            mHeatmapHandler.setRadius(HeatmapConstants.ALT_HEATMAP_RADIUS);
+            mHeatmapHelper.setRadius(HeatmapConstants.ALT_HEATMAP_RADIUS);
         }
         else {
-            mHeatmapHandler.setRadius(HeatmapConstants.DEFAULT_HEATMAP_RADIUS);
+            mHeatmapHelper.setRadius(HeatmapConstants.DEFAULT_HEATMAP_RADIUS);
         }
         defaultRadius =!defaultRadius;
     }
 
     public void changeGradient(View view) {
         if (defaultGradient) {
-            mHeatmapHandler.setGradient(HeatmapConstants.ALT_HEATMAP_GRADIENT);
+            mHeatmapHelper.setGradient(HeatmapConstants.ALT_HEATMAP_GRADIENT);
         }
         else {
-            mHeatmapHandler.setGradient(HeatmapConstants.DEFAULT_HEATMAP_GRADIENT);
+            mHeatmapHelper.setGradient(HeatmapConstants.DEFAULT_HEATMAP_GRADIENT);
         }
         defaultGradient = !defaultGradient;
     }
 
     public void changeOpacity(View view) {
         if (defaultOpacity) {
-            mHeatmapHandler.setOpacity(HeatmapConstants.ALT_HEATMAP_OPACITY);
+            mHeatmapHelper.setOpacity(HeatmapConstants.ALT_HEATMAP_OPACITY);
         }
         else {
-            mHeatmapHandler.setOpacity(HeatmapConstants.DEFAULT_HEATMAP_OPACITY);
+            mHeatmapHelper.setOpacity(HeatmapConstants.DEFAULT_HEATMAP_OPACITY);
         }
         defaultOpacity = !defaultOpacity;
     }
 
-    public void changeData(View view) {
-        if (origData) {
-            mHeatmapHandler.setData(mListThinned);
+    // Dealing with spinner choices
+    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            String dataset = parent.getItemAtPosition(pos).toString();
+            mHeatmapHelper.setData(mLists.get(dataset));
         }
-        else {
-            mHeatmapHandler.setData(mList);
-        }
-        origData = !origData;
 
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
     }
 
-    // https://explore.data.gov/Geography-and-Environment/EPA-FRS-Facilities-Combined-File-CSV-Download-for-/y38d-q6kk
-    private void readItems() throws JSONException {
+    // Datasets:
+    // Police Stations: all police stations across Australia from http://poidb.com
+    private ArrayList<LatLngWrapper> readItems(int resource) throws JSONException {
+        ArrayList<LatLngWrapper> list = new ArrayList<LatLngWrapper>();
         long start = getTime();
-        InputStream inputStream = getResources().openRawResource(R.raw.latlong_500);
+        InputStream inputStream = getResources().openRawResource(resource);
         String json = new Scanner(inputStream).useDelimiter("\\A").next();
         JSONArray array = new JSONArray(json);
         for (int i = 0; i < array.length(); i++) {
             JSONObject object = array.getJSONObject(i);
             double lat = object.getDouble("lat");
             double lng = object.getDouble("lng");
-            mList.add(new LatLngWrapper(new LatLng(lat, lng)));
-            if (i % 10 == 0) {
-                mListThinned.add(new LatLngWrapper(new LatLng(lat, lng)));
-            }
+            list.add(new LatLngWrapper(new LatLng(lat, lng)));
         }
 
         long end = getTime();
         Log.e("Time readItems", (end-start)+"ms");
+        return list;
     }
 
 
