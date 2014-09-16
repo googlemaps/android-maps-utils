d176ce381ac144593343d2981ab4ac53b016f843
Bob Hagemann <bob+code@twilio.com>
2014-09-16 09:20:35 -0700

    ConcurrentHashMap style changes

--- library/src/com/google/maps/android/clustering/view/DefaultClusterRenderer.java
+++ library/src/com/google/maps/android/clustering/view/DefaultClusterRenderer.java
@@ -71,7 +71,7 @@ public class DefaultClusterRenderer<T extends ClusterItem> implements ClusterRen
      * Markers that are currently on the map.
      */
     private Set<MarkerWithPosition> mMarkers = Collections.newSetFromMap(
-        new ConcurrentHashMap<MarkerWithPosition,Boolean>());
+            new ConcurrentHashMap<MarkerWithPosition, Boolean>());
 
     /**
      * Icons for each bucket.
@@ -362,7 +362,7 @@ public class DefaultClusterRenderer<T extends ClusterItem> implements ClusterRen
 
             // Create the new markers and animate them to their new positions.
             final Set<MarkerWithPosition> newMarkers = Collections.newSetFromMap(
-                new ConcurrentHashMap<MarkerWithPosition,Boolean>());
+                    new ConcurrentHashMap<MarkerWithPosition, Boolean>());
             for (Cluster<T> c : clusters) {
                 boolean onScreen = visibleBounds.contains(c.getPosition());
                 if (zoomingIn && onScreen && SHOULD_ANIMATE) {
