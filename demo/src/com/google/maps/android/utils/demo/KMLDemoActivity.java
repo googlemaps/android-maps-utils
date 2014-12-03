2b1cb5987c644d71b3d17e64dee01b7c0d54f4b7
Lavender Chan <lavenderc@google.com>
2014-12-03 16:44:29 +1100

    readItems is implemented, which takes a KML file and returns a parser to that file. Basic functionality is included to startDemo to see if it is working

--- demo/src/com/google/maps/android/utils/demo/KMLDemoActivity.java
+++ demo/src/com/google/maps/android/utils/demo/KMLDemoActivity.java
@@ -1,11 +1,26 @@
 package com.google.maps.android.utils.demo;
 
-/**
- * Created by juliawong on 12/3/14.
- */
-public class KMLDemoActivity extends BaseDemoActivity {
-    @Override
-    protected void startDemo() {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONException;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+
+public class KMLDemoActivity extends BaseDemoActivity {
+
+    private XmlPullParser readItems() throws JSONException, XmlPullParserException {
+        InputStream stream = getResources().openRawResource(R.raw.cta);
+        XmlPullParser parser = XmlPullParserFactory.newInstance().newPullParser();
+        parser.setInput(stream, null);
+        return parser;
+    }
+
+    protected void startDemo() {
+            System.out.println("Hello");
+            getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-33.8256, 151.2395), 3));
     }
 }
