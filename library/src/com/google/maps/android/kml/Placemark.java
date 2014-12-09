8f5089e1b7c1df81195451496ef81a22d59e1665
Lavender Chan <lavenderc@google.com>
2014-12-09 14:26:38 +1100

    Changed variable assignment, now hashmap

--- library/src/com/google/maps/android/kml/Placemark.java
+++ library/src/com/google/maps/android/kml/Placemark.java
@@ -2,88 +2,40 @@ package com.google.maps.android.kml;
 
 import org.xmlpull.v1.XmlPullParser;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by lavenderc on 12/3/14.
  */
 public class Placemark {
 
-    private String name;
-    private String visibility;
-    private String description;
-    private String styleURL;
-    private String address;
-    private String phoneNumber;
+    private ArrayList<Coordinate> line;
 
-    private int POLYGON_TYPE = 0;
-    private int LINESTRING_TYPE = 1;
-    private int POINT_TYPE = 2;
-
-    private int INNER_BOUNDARY = 0;
-    private int OUTER_BOUNDARY = 1;
+    private HashMap<String, String> values;
 
     public Placemark() {
-        name = null;
-        visibility = null;
-        description = null;
-        styleURL = null;
-        phoneNumber = null;
-        address = null;
-    }
-
-    public void setName(String string) {
-        name = string;
-    }
-    public void setVisibility(String string) {
-        visibility = string;
-    }
-    public void setDescription(String string) {
-        description = string;
-    }
-    public void setStyleURL(String string) {
-        styleURL = string;
-    }
-    public void setPhoneNumber(String string) {
-        phoneNumber = string;
-    }
-    public void setAddress(String string) {
-        address = string;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public String getVisibility() {
-        return visibility;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public String getStyleURL() {
-        return styleURL;
-    }
-    public String getAddress() {
-        return address;
-    }
-    public String getPhoneNumber() {
-        return phoneNumber;
+        line = null;
+        values = new HashMap<String, String>();
     }
 
 
-    public void setPolygon(String coordinates, int type) {
-        Coordinate coordinate = new Coordinate();
-        coordinate.setType(POLYGON_TYPE);
-        coordinate.setBoundary(type);
-        coordinate.setCoordinateList(coordinates);
+    public void setValues (String key, String value) {
+        values.put(key, value);
     }
-    public void setLineString(String coordinates) {
-        Coordinate coordinate = new Coordinate();
-        coordinate.setType(LINESTRING_TYPE);
-        coordinate.setCoordinateList(coordinates);
+
+    public String getValues (String key) {
+        return values.get(key);
     }
-    public void setPoint(String coordinates) {
-        Coordinate coordinate = new Coordinate();
-        coordinate.setType(POINT_TYPE);
-        coordinate.setCoordinateList(coordinates);
+
+    public void setLine(ArrayList<Coordinate> coordinates) {
+        line = coordinates;
     }
+    public ArrayList<Coordinate> getLine () {
+        return line;
+    }
+
+
 
 }
