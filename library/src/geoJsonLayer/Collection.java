9682a0fee2b16e9bec76d66c3553df5f811c7e5b
Julia Wong <juliawong0@gmail.com>
2014-12-29 14:33:41 +1100

    Added methods for collection class

--- library/src/geoJsonLayer/Collection.java
+++ library/src/geoJsonLayer/Collection.java
@@ -10,6 +10,51 @@ import java.util.ArrayList;
  * Created by juliawong on 12/29/14.
  */
 public class Collection {
+    private ArrayList<Feature> mFeatures = new ArrayList<Feature>();
+    private GoogleMap mMap;
 
+    public Collection(GoogleMap map, int resourceId, Context context) {
+        mMap = map;
+    }
+
+    // TODO: implement an iterator thing or just return mFeatures
+
+    public Feature getFeatureById(String id) {
+
+        return null;
+    }
+
+    public GoogleMap getMap() {
+        return mMap;
+    }
+
+    // TODO: Split styles or keep as one
+    public Style getStyle() {
+        return null;
+    }
+
+    public void removeFeature(Feature feature) {
+
+    }
+
+    public void setDefaultStyle(Style defaultStyle) {
+
+    }
+
+    public void setMap(GoogleMap map) {
+        mMap = map;
+    }
+
+    public void setPreserveViewPort(boolean preserveViewPort) {
+
+    }
+
+    public void setStyle(Style style) {
+
+    }
+
+    public void setZIndex(int zIndex) {
+
+    }
 
 }
