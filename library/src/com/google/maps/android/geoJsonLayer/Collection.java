90e822f65b910c7207f2e9c9d07031b49688601b
Julia Wong <juliawong0@gmail.com>
2014-12-30 15:38:31 +1100

    Added ability to read raw resource files

--- library/src/com/google/maps/android/geoJsonLayer/Collection.java
+++ library/src/com/google/maps/android/geoJsonLayer/Collection.java
@@ -2,8 +2,15 @@ package com.google.maps.android.geoJsonLayer;
 
 import com.google.android.gms.maps.GoogleMap;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.content.Context;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
@@ -11,7 +18,7 @@ import java.util.ArrayList;
  */
 public class Collection {
 
-    private ArrayList<Feature> mFeatures = new ArrayList<Feature>();
+    private ArrayList<Feature> mFeatures;
 
     private GoogleMap mMap;
 
@@ -21,8 +28,41 @@ public class Collection {
 
     private PolygonStyle mDefaultPolygonStyle;
 
-    public Collection(GoogleMap map, int resourceId, Context context) {
+    public Collection(GoogleMap map, JSONObject geoJsonObject)
+            throws IOException, JSONException {
         mMap = map;
+        mFeatures = new ArrayList<Feature>();
+    }
+
+    public Collection(GoogleMap map, int resourceId, Context context)
+            throws IOException, JSONException {
+        mMap = map;
+        mFeatures = new ArrayList<Feature>();
+        InputStream stream = context.getResources().openRawResource(resourceId);
+        JSONObject geoJsonObject = createJsonFileObject(stream);
+    }
+
+    /**
+     * Takes a character input stream and converts it into a JSONObject
+     *
+     * @param stream Character input stream representing  the GeoJSON file
+     * @return JSONObject representing the GeoJSON file
+     * @throws java.io.IOException   if the file cannot be opened for read
+     * @throws org.json.JSONException if the JSON file has poor structure
+     */
+    private JSONObject createJsonFileObject(InputStream stream) throws IOException, JSONException {
+        String line;
+        StringBuilder result = new StringBuilder();
+        // Reads from stream
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+        // Read each line of the GeoJSON file into a string
+        while ((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+
+        // Converts the result string into a JSONObject
+        return new JSONObject(result.toString());
     }
 
     // TODO: implement an iterator thing or just return mFeatures
