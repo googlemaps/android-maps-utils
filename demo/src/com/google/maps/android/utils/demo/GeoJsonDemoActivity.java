fa4993427ea168ed1ee0e1fe0730e43a77f4e8c2
Julia Wong <juliawong0@gmail.com>
2014-12-31 11:52:01 +1100

    Updated demo for new GeoJson utility

--- demo/src/com/google/maps/android/utils/demo/GeoJsonDemoActivity.java
+++ demo/src/com/google/maps/android/utils/demo/GeoJsonDemoActivity.java
@@ -1,10 +1,12 @@
 package com.google.maps.android.utils.demo;
 
-import com.google.maps.android.geoJsonImport.GeoJsonImport;
+import com.google.maps.android.geoJsonLayer.Collection;
+
+import org.json.JSONException;
 
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+
+import java.io.IOException;
 
 /**
  * Created by juliawong on 12/1/14.
@@ -18,53 +20,13 @@ public class GeoJsonDemoActivity extends BaseDemoActivity {
     @Override
     protected void startDemo() {
         try {
-
-            final GeoJsonImport featureCollectionTest = new GeoJsonImport(getMap(),
-                    R.raw.feature_collection_geojson_demo, getApplicationContext());
-
-            featureCollectionTest.addGeoJsonData();
-
-            final Button add_button = (Button) findViewById(R.id.add);
-            add_button.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    featureCollectionTest.addGeoJsonData();
-
-                }
-            });
-
-            final Button toggle_button = (Button) findViewById(R.id.toggle);
-            toggle_button.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    featureCollectionTest.toggleVisibility();
-
-                }
-            });
-
-            final Button show_all_button = (Button) findViewById(R.id.show_all);
-            show_all_button.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    featureCollectionTest.showAllGeoJsonData();
-                }
-            });
-
-            final Button hide_all_button = (Button) findViewById(R.id.hide_all);
-            hide_all_button.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    featureCollectionTest.hideAllGeoJsonData();
-                }
-            });
-
-            final Button remove_button = (Button) findViewById(R.id.remove);
-            remove_button.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    featureCollectionTest.removeGeoJsonData();
-                }
-            });
-
-
-
-        } catch (Exception e) {
-            Log.v("Exception", e.toString());
+            Collection collection = new Collection(getMap(), R.raw.markers_in_spain_geojson, getApplicationContext());
+            collection.parseGeoJson();
+            Log.i("Features", collection.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 }
