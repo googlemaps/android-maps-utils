1c4f0c96c65782acc4667882f5d6706ee3012e43
Julia Wong <juliawong0@gmail.com>
2015-01-05 14:40:46 +1100

    Added testing of ID

--- library/tests/src/com/google/maps/android/geoJsonLayer/GeoJsonParserTest.java
+++ library/tests/src/com/google/maps/android/geoJsonLayer/GeoJsonParserTest.java
@@ -1,6 +1,13 @@
 package com.google.maps.android.geoJsonLayer;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import junit.framework.TestCase;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GeoJsonParserTest extends TestCase {
 
@@ -14,7 +21,21 @@ public class GeoJsonParserTest extends TestCase {
     }
 
     public void testParseGeoJson() throws Exception {
+        JSONObject geoJsonObject = new JSONObject("{ \"type\": \"MultiLineString\",\n"
+                + "    \"coordinates\": [\n"
+                + "        [ [100.0, 0.0], [101.0, 1.0] ],\n"
+                + "        [ [102.0, 2.0], [103.0, 3.0] ]\n"
+                + "      ]\n"
+                + "    }");
 
+        GeoJsonParser parser = new GeoJsonParser(geoJsonObject);
+        parser.parseGeoJson();
+        LineString ls1 = new LineString(new ArrayList<LatLng>(Arrays.asList(new LatLng(0, 100), new LatLng(1, 101))));
+        LineString ls2 = new LineString(new ArrayList<LatLng>(Arrays.asList(new LatLng(2, 102), new LatLng(3, 103))));
+        MultiLineString multiLineString = new MultiLineString(new ArrayList<LineString>(Arrays.asList(ls1, ls2)));
+        Feature feature = new Feature(multiLineString, null, null);
+        ArrayList<Feature> features = new ArrayList<Feature>(Arrays.asList(feature));
+        assertEquals(features.get(0).getId(), parser.getFeatures().get(0).getId());
     }
 
     public void testGetFeatures() throws Exception {
