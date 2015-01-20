7a6438f855cdd2d7673adcb644c84cd280356e90
Julia Wong <juliawong0@gmail.com>
2015-01-20 16:52:03 +1100

    Added more GeoJsonFeature tests

--- library/tests/src/com/google/maps/android/geojson/GeoJsonFeatureTest.java
+++ library/tests/src/com/google/maps/android/geojson/GeoJsonFeatureTest.java
@@ -1,18 +1,20 @@
 package com.google.maps.android.geojson;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class GeoJsonFeatureTest extends TestCase {
 
-    public void setUp() throws Exception {
-        super.setUp();
-
-    }
+    GeoJsonFeature feature;
 
     public void testGetId() throws Exception {
-        GeoJsonFeature feature = new GeoJsonFeature(null, "Pirate", null, null);
+        feature = new GeoJsonFeature(null, "Pirate", null, null);
         assertNotNull(feature.getId());
         assertTrue(feature.getId().equals("Pirate"));
         feature = new GeoJsonFeature(null, null, null, null);
@@ -23,7 +25,7 @@ public class GeoJsonFeatureTest extends TestCase {
         HashMap<String, String> properties = new HashMap<String, String>();
         properties.put("Color", "Yellow");
         properties.put("Width", "5");
-        GeoJsonFeature feature = new GeoJsonFeature(null, null, properties, null);
+        feature = new GeoJsonFeature(null, null, properties, null);
         assertNotNull(feature.getProperties());
         for (String string : feature.getProperties().keySet()) {
             assertTrue(string.equals("Color") || string.equals("Width"));
@@ -34,10 +36,53 @@ public class GeoJsonFeatureTest extends TestCase {
     }
 
     public void testPointStyle() {
-        GeoJsonFeature feature = new GeoJsonFeature(null, null, null, null);
+        feature = new GeoJsonFeature(null, null, null, null);
         GeoJsonPointStyle pointStyle = new GeoJsonPointStyle();
         feature.setPointStyle(pointStyle);
-        assertTrue(pointStyle.equals(feature.getPointStyle()));
+        assertEquals(pointStyle, feature.getPointStyle());
+    }
+
+    public void testLineStringStyle() {
+        feature = new GeoJsonFeature(null, null, null, null);
+        GeoJsonLineStringStyle lineStringStyle = new GeoJsonLineStringStyle();
+        feature.setLineStringStyle(lineStringStyle);
+        assertEquals(lineStringStyle, feature.getLineStringStyle());
+    }
+
+    public void testPolygonStyle() {
+        feature = new GeoJsonFeature(null, null, null, null);
+        GeoJsonPolygonStyle polygonStyle = new GeoJsonPolygonStyle();
+        feature.setPolygonStyle(polygonStyle);
+        assertEquals(polygonStyle, feature.getPolygonStyle());
+    }
+
+    public void testGeometry() {
+        feature = new GeoJsonFeature(null, null, null, null);
+        assertNull(feature.getGeometry());
+        GeoJsonPoint point = new GeoJsonPoint(new LatLng(0, 0));
+        feature.setGeometry(point);
+        assertEquals(point, feature.getGeometry());
+        feature.setGeometry(null);
+        assertNull(feature.getGeometry());
+
+        GeoJsonLineString lineString = new GeoJsonLineString(new ArrayList<LatLng>(Arrays.asList(new LatLng(0, 0), new LatLng(50, 50))));
+        feature = new GeoJsonFeature(lineString, null, null, null);
+        assertEquals(lineString, feature.getGeometry());
+        feature.setGeometry(point);
+        assertEquals(point, feature.getGeometry());
+        feature.setGeometry(null);
+        assertNull(feature.getGeometry());
+        feature.setGeometry(lineString);
+        assertEquals(lineString, feature.getGeometry());
+    }
+
+    public void testGetBoundingBox() {
+        feature = new GeoJsonFeature(null, null, null, null);
+        assertNull(feature.getBoundingBox());
+
+        LatLngBounds boundingBox = new LatLngBounds(new LatLng(-20, -20), new LatLng(50, 50));
+        feature = new GeoJsonFeature(null, null, null, boundingBox);
+        assertEquals(boundingBox, feature.getBoundingBox());
     }
 
 
