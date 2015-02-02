5bc9c408186367983e8071164eee49871e6d9f74
Julia Wong <juliawong0@gmail.com>
2015-02-02 14:23:23 +1100

    Added skeleton for renderer

--- library/tests/src/com/google/maps/android/geojson/GeoJsonRendererTest.java
+++ library/tests/src/com/google/maps/android/geojson/GeoJsonRendererTest.java
@@ -4,5 +4,36 @@ import junit.framework.TestCase;
 
 public class GeoJsonRendererTest extends TestCase {
 
+    public void setUp() throws Exception {
+        super.setUp();
 
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testGetMap() throws Exception {
+
+    }
+
+    public void testSetMap() throws Exception {
+
+    }
+
+    public void testGetFeatures() throws Exception {
+
+    }
+
+    public void testAddFeature() throws Exception {
+
+    }
+
+    public void testRemoveLayerFromMap() throws Exception {
+
+    }
+
+    public void testRemoveFeature() throws Exception {
+
+    }
 }
