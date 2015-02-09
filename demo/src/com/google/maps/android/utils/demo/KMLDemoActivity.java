eb7db2fb7ad27b684864f04bb6686f11d376fc14
Julia Wong <juliawong0@gmail.com>
2015-02-09 13:53:38 +1100

    Revert "Made code neater, seperated methods"
    This reverts commit 57f6b0c0e1b6b5c4df07106059be2d53fa1c75cb.

--- demo/src/com/google/maps/android/utils/demo/KMLDemoActivity.java
+++ demo/src/com/google/maps/android/utils/demo/KMLDemoActivity.java
@@ -2,38 +2,29 @@ package com.google.maps.android.utils.demo;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.HashMap;
 
-/*
- * Copyright 2014 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/**
+ * Created by lavenderc on 12/1/14.
  */
 public class KMLDemoActivity extends FragmentActivity {
 
     private GoogleMap mMap;
 
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
@@ -61,39 +52,9 @@ public class KMLDemoActivity extends FragmentActivity {
     }
 
     /**********************************
-     Reads a KML file and retrieves data based on the tag and the content of the tag.
-     Currently reads input from an input stream and stops when an end of document tag,
-     </kml>,is reached. Currently method only supports <coordinate></coordinate>
-     **********************************/
-
-    public void setKML () {
-        PolylineOptions options = new PolylineOptions();
-
-        try {
-            XmlPullParser parser = getKMLFile();
-            int eventType = parser.getEventType();
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                if (eventType == XmlPullParser.START_TAG) {
-                    if (parser.getName().equals("coordinates")) {
-                        options = getPlotPoints(parser);
-                    }
-                }
-                eventType = parser.next();
-                mMap.addPolyline(options);
-                options = new PolylineOptions();
-            }
-        } catch (Exception e) {
-            String error = "Unable to retrieve KML file. " +
-                    "Make sure that the file is in the correct destination folder.";
-            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
-        }
-    }
-
-    /**********************************
-        Returns an XML Parser from a given KML or XML data set and returns as an input stream.
+        Returns an XML Parser from a given KML or XML data set.
         To change the file destination, change R.raw.<Your file here>
         Raw data is currently being fetched from demo/res/raw folder.
-        To retrieve a file from a URL, uncomment and replace YOUR_URL_HERE with your own.
      **********************************/
 
     private XmlPullParser getKMLFile () {
@@ -102,51 +63,63 @@ public class KMLDemoActivity extends FragmentActivity {
         try {
             parser = XmlPullParserFactory.newInstance().newPullParser();
             stream = getResources().openRawResource(R.raw.cta);
-            //stream = new URL("YOUR_URL_HERE").openStream();
             parser.setInput(stream, null);
         } catch (Exception e) {
-            String error = "Unable to retrieve KML file. " +
-                    "Make sure that the file is in the correct destination folder.";
-            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+            System.out.println("File not found!");
         }
         return parser;
     }
 
     /**********************************
-        Receives a list of coordinates from KML data and returns a PolylineOption class.
-        The method receives plain text coordinates from an input steam, then splits by line
-        to get a coordinate set, then splits by comma to get latitude and longitude values.
+     Receives a list of coordinates from KML data and turns them into a polyline option
      **********************************/
 
     private PolylineOptions getPlotPoints(XmlPullParser parser) {
         String[] lines;
         PolylineOptions options = new PolylineOptions();
+        Double lonDouble, latDouble;
+        Integer lon = 0, lat = 1;
         LatLng latLng;
         try {
             lines = parser.nextText().split("\n");
             for (String point: lines) {
                 String[] coordinate = point.split(",");
                 if (coordinate.length > 2) {
-                    Double latDouble = Double.parseDouble(coordinate[1]);
-                    Double lonDouble = Double.parseDouble(coordinate[0]);
-                    if (lonDouble > 120 || lonDouble < -120 || latDouble > 90 || latDouble < 90) {
-                        String error = "KML data provided is in the incorrect format. " +
-                                "Expected: lon, lat. Given: lat, lon";
-                        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
-                        break;
-                    }
+                    latDouble = Double.parseDouble(coordinate[lat]);
+                    lonDouble = Double.parseDouble(coordinate[lon]);
                     latLng = new LatLng(latDouble,lonDouble);
                     options.add(latLng).width(4).color(Color.BLACK);
                 }
             }
         } catch (Exception e) {
-            String error = "Unable to retrieve list of coordinates. " +
-                    "Make sure that there is content between the coordinate tags.";
-            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+            System.out.println ("Coordinate list not found! Try again");
         }
         return options;
     }
 
 
+    public void setKML () {
+
+        PolylineOptions options = new PolylineOptions();
+        String name;
+
+        try {
+            XmlPullParser parser = getKMLFile();
+            int eventType = parser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG) {
+                    name = parser.getName();
+                    if (name.equals("coordinates")) {
+                        options = getPlotPoints(parser);
+                    }
+                }
+                eventType = parser.next();
+                mMap.addPolyline(options);
+                options = new PolylineOptions();
+            }
+        } catch (Exception e) {
+            System.out.println("Nope");
+        }
+    }
 
 }
