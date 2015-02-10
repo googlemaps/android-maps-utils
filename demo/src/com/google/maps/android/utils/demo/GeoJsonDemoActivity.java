39fc096638f51a7d0bbb6d284939df2402cbdb61
Julia Wong <juliawong0@gmail.com>
2015-02-10 14:08:16 +1100

    Updated demo

--- demo/src/com/google/maps/android/utils/demo/GeoJsonDemoActivity.java
+++ demo/src/com/google/maps/android/utils/demo/GeoJsonDemoActivity.java
@@ -1,13 +1,22 @@
 package com.google.maps.android.utils.demo;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.geojson.GeoJsonFeature;
 import com.google.maps.android.geojson.GeoJsonLayer;
+import com.google.maps.android.geojson.GeoJsonPoint;
 import com.google.maps.android.geojson.GeoJsonPolygonStyle;
+
+import org.json.JSONException;
 
 import android.graphics.Color;
 import android.util.Log;
 
+import java.io.IOException;
+import java.util.HashMap;
+
 public class GeoJsonDemoActivity extends BaseDemoActivity {
+
+    private final static String mLogTag = "GeoJsonDemo";
 
     protected int getLayoutId() {
         return R.layout.geojson_demo;
@@ -15,7 +24,6 @@ public class GeoJsonDemoActivity extends BaseDemoActivity {
 
     @Override
     protected void startDemo()  {
-        // TODO: make a demo
         try {
             GeoJsonLayer layer = new GeoJsonLayer(getMap(), R.raw.google, getApplicationContext());
             layer.addLayer();
@@ -26,8 +34,17 @@ public class GeoJsonDemoActivity extends BaseDemoActivity {
                     feature.setPolygonStyle(polygonStyle);
                 }
             }
-        } catch (Exception e) {
-            Log.e("ERROR", e.getMessage());
+            GeoJsonPoint point = new GeoJsonPoint(new LatLng(-33.866670, 151.195826));
+            HashMap<String, String> properties = new HashMap<String, String>();
+            properties.put("country", "Australia");
+            properties.put("city", "Sydney");
+            GeoJsonFeature feature = new GeoJsonFeature(point, "Google Sydney", properties, null);
+            layer.addFeature(feature);
+
+        } catch (JSONException e) {
+            Log.e(mLogTag, "JSON Exception caught: " + e.toString());
+        } catch (IOException e) {
+            Log.e(mLogTag, "IO Exception caught: " + e.toString());
         }
     }
 }
