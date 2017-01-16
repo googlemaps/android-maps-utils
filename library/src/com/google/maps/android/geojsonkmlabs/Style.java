8d7cf7a0f4f3ae430860f9f96db8245b759fc8ae
Elizabeth Stevenson <libbae@libbae.syd.corp.google.com>
2017-01-16 13:56:51 +1100

    Made class description comment a javadoc comment in Layer and Style.

--- library/src/com/google/maps/android/geojsonkmlabs/Style.java
+++ library/src/com/google/maps/android/geojsonkmlabs/Style.java
@@ -6,9 +6,9 @@ import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.Observable;
 
-/*
-An abstraction that shares the common properties of KmlStyle and GeoJsonLoneStringStyle,
-GeoJsonPointStyle and GeoJsonPolygonStyle
+/**
+ * An abstraction that shares the common properties of KmlStyle and GeoJsonLoneStringStyle,
+ * GeoJsonPointStyle and GeoJsonPolygonStyle
  */
 public abstract class Style extends Observable {
 
