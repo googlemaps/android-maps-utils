e3926d5a9ad1a766f99f155849e7b54b0bd43999
israel@cobaas.com <israel.menis@outlook.com>
2017-07-09 22:30:52 +0200

    Added new demo activity showing 5 markers in the same location.

---
+++ demo/src/com/google/maps/android/utils/demo/ClusteringSameLocationActivity.java
@@ -0,0 +1,38 @@
+package com.google.maps.android.utils.demo;
+
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.android.utils.demo.model.MyItem;
+
+import org.json.JSONException;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClusteringSameLocationActivity extends BaseDemoActivity {
+    private ClusterManager<MyItem> mClusterManager;
+
+    @Override
+    protected void startDemo() {
+        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.503186, -0.126446), 10));
+
+        mClusterManager = new ClusterManager<MyItem>(this, getMap());
+        getMap().setOnCameraIdleListener(mClusterManager);
+
+        try {
+            readItems();
+        } catch (JSONException e) {
+            Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void readItems() throws JSONException {
+        InputStream inputStream = getResources().openRawResource(R.raw.markers_same_location);
+        List<MyItem> items = new MyItemReader().read(inputStream);
+        mClusterManager.addItems(items);
+    }
+}
