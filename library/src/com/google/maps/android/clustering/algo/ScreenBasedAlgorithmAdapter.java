cf624829616d9f93defe1bcf5f7fd151723fdd5e
Stephen McDonald <steve@jupo.org>
2018-04-18 11:29:45 +1000

    Add max distance methods to ScreenBasedAlgorithmAdapter

--- library/src/com/google/maps/android/clustering/algo/ScreenBasedAlgorithmAdapter.java
+++ library/src/com/google/maps/android/clustering/algo/ScreenBasedAlgorithmAdapter.java
@@ -67,7 +67,18 @@ public class ScreenBasedAlgorithmAdapter<T extends ClusterItem> implements Scree
     }
 
     @Override
+    public void setMaxDistanceBetweenClusteredItems(int maxDistance) {
+        mAlgorithm.setMaxDistanceBetweenClusteredItems(maxDistance);
+    }
+
+    @Override
+    public int getMaxDistanceBetweenClusteredItems() {
+        return mAlgorithm.getMaxDistanceBetweenClusteredItems();
+    }
+
+    @Override
     public void onCameraChange(CameraPosition cameraPosition) {
         // stub
     }
+
 }
