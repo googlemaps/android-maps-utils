0005ae733efa8770e1669124a9da40b01cb30b70
Sean <Qwatcha@users.noreply.github.com>
2018-05-01 07:17:35 +0100

    Set polygon clickable option with style instead of forced true. (#454)
    (cherry picked from commit 253f099)

--- library/src/com/google/maps/android/data/Renderer.java
+++ library/src/com/google/maps/android/data/Renderer.java
@@ -423,11 +423,11 @@ public class Renderer {
      *
      * @param feature feature to add to the map
      */
-     public void addFeature(Feature feature) {
+    public void addFeature(Feature feature) {
         Object mapObject = FEATURE_NOT_ON_MAP;
-         if (feature instanceof GeoJsonFeature) {
-             setFeatureDefaultStyles((GeoJsonFeature) feature);
-         }
+        if (feature instanceof GeoJsonFeature) {
+            setFeatureDefaultStyles((GeoJsonFeature) feature);
+        }
         if (mLayerOnMap) {
             if (mFeatures.containsKey(feature)) {
                 // Remove current map objects before adding new ones
@@ -637,7 +637,7 @@ public class Renderer {
      * @return Polyline object created from given LineString
      */
     protected Polyline addLineStringToMap(PolylineOptions polylineOptions,
-                                       LineString lineString) {
+                                          LineString lineString) {
         // Add coordinates
         polylineOptions.addAll(lineString.getGeometryObject());
         Polyline addedPolyline = mMap.addPolyline(polylineOptions);
@@ -680,7 +680,7 @@ public class Renderer {
             polygonOptions.addHole(innerBoundary);
         }
         Polygon addedPolygon = mMap.addPolygon(polygonOptions);
-        addedPolygon.setClickable(true);
+        addedPolygon.setClickable(polygonOptions.isClickable());
         return addedPolygon;
     }
 
