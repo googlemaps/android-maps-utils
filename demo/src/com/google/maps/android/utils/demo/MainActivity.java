b0a6ce8998aa53fdf83570fef84649433ff70364
Jeff Lockhart <jeff@salesrabbit.com>
2019-03-23 23:35:07 -0600

    Add ViewModel demo

--- demo/src/com/google/maps/android/utils/demo/MainActivity.java
+++ demo/src/com/google/maps/android/utils/demo/MainActivity.java
@@ -33,12 +33,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        mListView = (ViewGroup) findViewById(R.id.list);
+        mListView = findViewById(R.id.list);
 
         addDemo("Clustering", ClusteringDemoActivity.class);
         addDemo("Clustering: Custom Look", CustomMarkerClusteringDemoActivity.class);
         addDemo("Clustering: 2K markers", BigClusteringDemoActivity.class);
-        addDemo("Clustering: 20k only visible markers", VisibleClusteringDemoActivity.class);
+        addDemo("Clustering: 20K only visible markers", VisibleClusteringDemoActivity.class);
+        addDemo("Clustering: ViewModel", ClusteringViewModelDemoActivity.class);
         addDemo("PolyUtil.decode", PolyDecodeDemoActivity.class);
         addDemo("PolyUtil.simplify", PolySimplifyDemoActivity.class);
         addDemo("IconGenerator", IconGeneratorDemoActivity.class);
