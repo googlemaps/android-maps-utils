4a01fe6a0002b1504d5f4894c4ecb79094ef161e
Sean Barbeau <barbeau@cutr.usf.edu>
2020-01-30 12:28:01 -0500

    Add unit tests for KML/GeoJSON clickable
    * For GeoJSON - test both default and programmatic custom clickable styles for both the style and renderer classes
    * For KML - test the default style (programmatic custom styles aren't yet supported) for both the style and renderer classes
    * Refactor KML and GeoJSON utility method used in more than one test class to new Kml/GeoJsonTestUtil classes
    * Fix GeoJSON polygonStyle.toPolygonOptions().isVisible() test (this previously didn't check PolygonOptions)

---
+++ library/src/androidTest/java/com/google/maps/android/data/kml/KmlTestUtil.java
@@ -0,0 +1,37 @@
+package com.google.maps.android.data.kml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import androidx.annotation.IdRes;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+/**
+ * Shared utilities for KML tests
+ */
+public class KmlTestUtil {
+
+    /**
+     * Returns an XmlPullParser for the given KML file
+     * @param res the resource ID for a KML file
+     * @return an XmlPullParser for the given KML file
+     * @throws XmlPullParserException, IOException
+     */
+    static XmlPullParser createParser(@IdRes int res) throws XmlPullParserException, IOException {
+        InputStream stream =
+                InstrumentationRegistry.getInstrumentation()
+                        .getTargetContext()
+                        .getResources()
+                        .openRawResource(res);
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+        XmlPullParser parser = factory.newPullParser();
+        parser.setInput(stream, null);
+        parser.next();
+        return parser;
+    }
+}
