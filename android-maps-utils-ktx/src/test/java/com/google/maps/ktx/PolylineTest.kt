f0e6c6e5ac7951b6c31f03934fc9231c0e16cb87
Chris Arriola <carriola@google.com>
2020-02-13 08:16:01 -0800

    Adding unit tests.

--- android-maps-utils-ktx/src/test/java/com/google/maps/ktx/PolylineTest.kt
+++ android-maps-utils-ktx/src/test/java/com/google/maps/ktx/PolylineTest.kt
@@ -8,22 +8,32 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class PolylineTest {
+    private val earthRadius = 6371009.0
+
     @Test
-    fun testContainsTrue() {
+    fun `test that contains returns true`() {
         val line = mockPolyline(listOf(LatLng(1.0, 0.0), LatLng(3.0, 0.0)))
         assertTrue(line.contains(LatLng(2.0, 0.0)))
     }
 
     @Test
-    fun testContainsTrueWithTolerance() {
+    fun `test that contains returns true with tolerance`() {
         val line = mockPolyline(listOf(LatLng(1.0, 0.0), LatLng(3.0, 0.0)))
         assertTrue(line.contains(LatLng(1.0, 0.00000001)))
     }
 
     @Test
-    fun testContainsFalse() {
+    fun `test that contains returns false`() {
         val line = mockPolyline(listOf(LatLng(1.0, 0.0), LatLng(3.0, 0.0)))
         assertFalse(line.contains(LatLng(4.0, 0.0)))
+    }
+
+    @Test
+    fun `validate spherical path length`() {
+        assertEquals(0, mockPolyline(emptyList()).sphericalPathLength)
+        val polyline = mockPolyline(listOf(LatLng(0.0, 0.0), LatLng(0.1, 0.1)))
+        val expectation = earthRadius * Math.sqrt(2.0) * Math.toRadians(0.1)
+        assertEquals(expectation, polyline.sphericalPathLength, 1e-6)
     }
 
     private fun mockPolyline(p: List<LatLng>, geodesic: Boolean = true) = mock<Polyline> {
