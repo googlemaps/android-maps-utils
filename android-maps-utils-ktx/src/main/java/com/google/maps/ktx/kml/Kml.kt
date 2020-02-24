fa1f8396cb23824a796e402a387107063c0da26a
Chris Arriola <carriola@google.com>
2020-02-24 09:43:38 -0800

    Using Context instead of FragmentActivity.

--- android-maps-utils-ktx/src/main/java/com/google/maps/ktx/kml/Kml.kt
+++ android-maps-utils-ktx/src/main/java/com/google/maps/ktx/kml/Kml.kt
@@ -1,12 +1,13 @@
 package com.google.maps.ktx.kml
 
+import android.content.Context
 import androidx.annotation.RawRes
-import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.maps.android.collections.GroundOverlayManager
 import com.google.maps.android.collections.MarkerManager
 import com.google.maps.android.collections.PolygonManager
 import com.google.maps.android.collections.PolylineManager
+import com.google.maps.android.data.Renderer
 import com.google.maps.android.data.kml.KmlLayer
 import java.io.InputStream
 
@@ -16,19 +17,21 @@ import java.io.InputStream
 inline fun kmlLayer(
     map: GoogleMap,
     @RawRes resourceId: Int,
-    activity: FragmentActivity,
+    context: Context,
     markerManager: MarkerManager = MarkerManager(map),
     polygonManager: PolygonManager = PolygonManager(map),
     polylineManager: PolylineManager = PolylineManager(map),
-    groundOverlayManager: GroundOverlayManager = GroundOverlayManager(map)
+    groundOverlayManager: GroundOverlayManager = GroundOverlayManager(map),
+    imagesCache: Renderer.ImagesCache? = null
 ) : KmlLayer = KmlLayer(
     map,
     resourceId,
-    activity,
+    context,
     markerManager,
     polygonManager,
     polylineManager,
-    groundOverlayManager
+    groundOverlayManager,
+    imagesCache
 )
 
 /**
@@ -37,17 +40,19 @@ inline fun kmlLayer(
 inline fun kmlLayer(
     map: GoogleMap,
     stream: InputStream,
-    activity: FragmentActivity,
+    context: Context,
     markerManager: MarkerManager = MarkerManager(map),
     polygonManager: PolygonManager = PolygonManager(map),
     polylineManager: PolylineManager = PolylineManager(map),
-    groundOverlayManager: GroundOverlayManager = GroundOverlayManager(map)
+    groundOverlayManager: GroundOverlayManager = GroundOverlayManager(map),
+    imagesCache: Renderer.ImagesCache? = null
 ) : KmlLayer = KmlLayer(
     map,
     stream,
-    activity,
+    context,
     markerManager,
     polygonManager,
     polylineManager,
-    groundOverlayManager
+    groundOverlayManager,
+    imagesCache
 )
