57a510f5fec6f8c39553f22c3a398988d05aff86
Sean Barbeau <sjbarbeau@gmail.com>
2020-07-22 17:36:59 -0400

    chore: Remove "Cross-track distance" algorithm and test data links

--- library/src/main/java/com/google/maps/android/PolyUtil.java
+++ library/src/main/java/com/google/maps/android/PolyUtil.java
@@ -503,23 +503,6 @@ public class PolyUtil {
         }
         LatLng su = new LatLng(start.latitude + u * (end.latitude - start.latitude), start.longitude + u * (end.longitude - start.longitude));
         return computeDistanceBetween(p, su);
-
-//        // "Cross-track distance" distance formula from https://www.movable-type.co.uk/scripts/latlong.html
-//        double distanceStartP = computeDistanceBetween(start, p) / EARTH_RADIUS;
-//        double bearingStartP = toRadians(computeHeading(start, p));
-//        double bearingStartEnd = toRadians(computeHeading(start, end));
-//
-//        double axt = Math.asin(Math.sin(distanceStartP) * Math.sin(bearingStartP - bearingStartEnd));
-//
-//        // Distance from P to great-circle path defined by start and end points
-//        double crossTrackDistance = axt * EARTH_RADIUS;
-//
-//        //  Distance from the start point to the closest point on the path to P
-//        double alongTrackDistance = Math.acos(Math.cos(distanceStartP) / Math.cos(crossTrackDistance / EARTH_RADIUS)) * EARTH_RADIUS;
-//
-//        // to compute points known distance from a great circle - http://www.edwilliams.org/avform.htm#XTE (also different cross and along track error formulas)
-//
-//        return Math.abs(crossTrackDistance);
     }
 
     /**
