269ef009d850a71348e7fbf393edc71a0b9c8891
Chris Arriola <carriola@google.com>
2020-11-18 09:42:57 -0800

    Detecting more methods.

--- lint-checks/src/main/java/com/google/maps/android/lint/checks/GoogleMapDetector.kt
+++ lint-checks/src/main/java/com/google/maps/android/lint/checks/GoogleMapDetector.kt
@@ -29,24 +29,34 @@ import org.jetbrains.uast.UCallExpression
 @Suppress("UnstableApiUsage")
 class GoogleMapDetector : Detector(), SourceCodeScanner  {
     override fun getApplicableMethodNames(): List<String>? =
-        listOf("setInfoWindowAdapter")
+        listOf(
+            "setInfoWindowAdapter",
+            "setOnInfoWindowClickListener",
+            "setOnInfoWindowLongClickListener",
+            "setOnMarkerClickListener",
+            "setOnMarkerDragListener",
+        )
 
     override fun visitMethodCall(context: JavaContext, node: UCallExpression, method: PsiMethod) {
         val evaluator = context.evaluator
         if (!evaluator.isMemberInClass(method, "com.google.android.gms.maps.GoogleMap")) {
             return
         }
-        context.report(POTENTIAL_BEHAVIOR_OVERRIDE, node, context.getLocation(node), POTENTIAL_BEHAVIOR_OVERRIDE.getBriefDescription(TextFormat.TEXT))
+        context.report(
+            issue = POTENTIAL_BEHAVIOR_OVERRIDE,
+            scope = node,
+            location = context.getLocation(node),
+            message = POTENTIAL_BEHAVIOR_OVERRIDE.getBriefDescription(TextFormat.TEXT)
+        )
     }
 
     companion object {
         val POTENTIAL_BEHAVIOR_OVERRIDE = Issue.create(
             id = "PotentialBehaviorOverride",
-            briefDescription = """
-                Using this method may override behaviors set by the Maps SDK for Android Utility 
-                Library. If you are not using clustering, GeoJson, or KML, you can safely suppress
-                this warning, otherwise, refer to the utility library's migration guide."
-            """.trimIndent(),
+            briefDescription = "Using this method may override behaviors set by the Maps SDK for " +
+                "Android Utility Library. If you are not using clustering, GeoJson, or KML, you " +
+                "can safely suppress this warning, otherwise, refer to the utility " +
+                "library's migration guide.",
             explanation = """
                 This lint warns for potential behavior override while using clustering, GeoJson, or
                 KML since these features use this method in their internal implementations. As such,
--- lint-checks/src/test/java/com/googe/maps/android/lint/checks/GoogleMapDetectorTest.kt
+++ lint-checks/src/test/java/com/googe/maps/android/lint/checks/GoogleMapDetectorTest.kt
@@ -21,17 +21,88 @@ import com.google.maps.android.lint.checks.GoogleMapDetector
 
 @Suppress("UnstableApiUsage")
 class GoogleMapDetectorTest : LintDetectorTest() {
-    fun testBasic() {
+
+    fun testSetOnMarkerDragListener() {
         lint().files(
-            java("""
-                package test.pkg;
-                
-                import com.google.android.gms.maps.GoogleMap;
-                
-                public class TestClass {
-                    private GoogleMap map;
-                    
-                    private void foo() { 
+            testFile(
+                """
+                        map.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+                            @Override 
+                            public void onMarkerDragStart(Marker marker) {
+                            }
+                            
+                            @Override 
+                            public void onMarkerDrag(Marker marker) {
+                            }
+                            
+                            @Override 
+                            public void onMarkerDragEnd(Marker marker) {
+                            }
+                      });
+            """
+            ),
+            java(GOOGLE_MAP_STUB).indented()
+        )
+            .run()
+            .expect(expectedText("map.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {"))
+    }
+
+    fun testSetOnMarkerClickListener() {
+        lint().files(
+            testFile(
+                """
+                        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                            @Override 
+                            public boolean onMarkerClick(Marker marker) {
+                                return false;
+                            }
+                      });
+            """
+            ),
+            java(GOOGLE_MAP_STUB).indented()
+        )
+            .run()
+            .expect(expectedText("map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {"))
+    }
+
+    fun testSetOnInfoWindowLongClickListener() {
+        lint().files(
+            testFile(
+                """
+                        map.setOnInfoWindowLongClickListener(new GoogleMap.OnInfoWindowLongClickListener() {
+                            @Override 
+                            public void onInfoWindowLongClick(Marker marker) {
+                            }
+                      });
+            """
+            ),
+            java(GOOGLE_MAP_STUB).indented()
+        )
+            .run()
+            .expect(expectedText("map.setOnInfoWindowLongClickListener(new GoogleMap.OnInfoWindowLongClickListener() {"))
+    }
+
+    fun testSetOnInfoWindowClickListener() {
+        lint().files(
+            testFile(
+                """
+                        map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                            @Override 
+                            public void onInfoWindowClick(Marker marker) {
+                            }
+                      });
+            """
+            ),
+            java(GOOGLE_MAP_STUB).indented()
+        )
+            .run()
+            .expect(expectedText("map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {"))
+    }
+
+    fun testSetInfoWindowAdapter() {
+        lint().files(
+            testFile(
+                """
                         map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
                             @Override 
                             public View getInfoWindow(Marker marker) {
@@ -43,21 +114,38 @@ class GoogleMapDetectorTest : LintDetectorTest() {
                                 return null;
                             }
                       });
+            """
+            ),
+            java(GOOGLE_MAP_STUB).indented()
+        )
+            .run()
+            .expect(expectedText("map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {"))
+    }
+
+    private fun expectedText(text: String): String =
+        """
+        src/test/pkg/TestClass.java:10: Warning: Using this method may override behaviors set by the Maps SDK for Android Utility Library. If you are not using clustering, GeoJson, or KML, you can safely suppress this warning, otherwise, refer to the utility library's migration guide. [PotentialBehaviorOverride]
+                $text
+                ^
+        0 errors, 1 warnings
+        """
+
+    private fun testFile(containing: String): TestFile {
+        return java(
+            """
+                package test.pkg;
+                
+                import com.google.android.gms.maps.GoogleMap;
+                
+                public class TestClass {
+                    private GoogleMap map;
+                    
+                    private void foo() { 
+                        $containing
                     }
                 }
             """
-            ).indented(),
-            java(GOOGLE_MAP_STUB).indented())
-            .run()
-            .expect("""
-                    src/test/pkg/TestClass.java:9: Warning: Using this method may override behaviors set by the Maps SDK for Android Utility 
-                    Library. If you are not using clustering, GeoJson, or KML, you can safely suppress
-                    this warning, otherwise, refer to the utility library's migration guide." [PotentialBehaviorOverride]
-                            map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-                            ^
-                    0 errors, 1 warnings
-                    """
-            )
+        ).indented()
     }
 
     override fun getDetector(): Detector {
@@ -78,6 +166,41 @@ class GoogleMapDetectorTest : LintDetectorTest() {
         public class GoogleMap {
            public final void setInfoWindowAdapter(GoogleMap.InfoWindowAdapter adapter) {
              // STUB
+           }
+
+           public final void setOnInfoWindowClickListener(GoogleMap.OnInfoWindowClickListener l) {
+             // STUB
+           }
+           
+           public final void setOnInfoWindowLongClickListener(GoogleMap.OnInfoWindowLongClickListener l) {
+             // STUB
+           }
+           
+           public final void setOnMarkerClickListener(GoogleMap.OnMarkerClickListener l) {
+             // STUB
+           }
+           
+           
+           public final void setOnMarkerDragListener(GoogleMap.OnMarkerDragListener l) {
+             // STUB
+           }
+           
+           public interface OnMarkerDragListener {
+             void onMarkerDragStart(Marker marker);
+             void onMarkerDrag(Marker marker);
+             void onMarkerDragEnd(Marker marker);
+           }
+           
+           public interface OnMarkerClickListener {
+             boolean onMarkerClick(Marker marker);
+           }
+           
+           public interface OnInfoWindowLongClickListener {
+             void onInfoWindowLongClick(Marker marker);
+           }
+           
+           public interface OnInfoWindowClickListener {
+             void onInfoWindowClick(Marker marker);
            }
            
            public interface InfoWindowAdapter {
