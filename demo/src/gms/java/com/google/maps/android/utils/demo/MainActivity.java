57b25619062f59ba28a2a6b77c674bd5574646e1
Chris Arriola <carriola@google.com>
2021-01-27 14:55:22 -0800

    chore: Use secrets_gradle_plugin.

--- demo/src/gms/java/com/google/maps/android/utils/demo/MainActivity.java
+++ demo/src/gms/java/com/google/maps/android/utils/demo/MainActivity.java
@@ -34,8 +34,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        if (getString(R.string.maps_api_key).isEmpty()) {
-            Toast.makeText(this, "Add your own API key in demo/secure.properties as MAPS_API_KEY=YOUR_API_KEY", Toast.LENGTH_LONG).show();
+        if (BuildConfig.MAPS_API_KEY.isEmpty()) {
+            Toast.makeText(this, "Add your own API key in local.properties as MAPS_API_KEY=YOUR_API_KEY", Toast.LENGTH_LONG).show();
         }
 
         mListView = findViewById(R.id.list);
