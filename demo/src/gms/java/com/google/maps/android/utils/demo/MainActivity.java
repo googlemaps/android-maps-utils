2d04a6b1af03b3464ddacd0fae82f633562c6b51
Sean Barbeau <sjbarbeau@gmail.com>
2021-09-29 16:08:24 -0400

    chore: Add overridable shouldRender(), update demo Activities
    * Restore CustomMarkerClusteringDemoActivity, add ZoomClusteringDemoActivity

--- demo/src/gms/java/com/google/maps/android/utils/demo/MainActivity.java
+++ demo/src/gms/java/com/google/maps/android/utils/demo/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         addDemo("Clustering: 2K markers", BigClusteringDemoActivity.class);
         addDemo("Clustering: 20K only visible markers", VisibleClusteringDemoActivity.class);
         addDemo("Clustering: ViewModel", ClusteringViewModelDemoActivity.class);
+        addDemo("Clustering: Force On Zoom", ZoomClusteringDemoActivity.class);
         addDemo("PolyUtil.decode", PolyDecodeDemoActivity.class);
         addDemo("PolyUtil.simplify", PolySimplifyDemoActivity.class);
         addDemo("IconGenerator", IconGeneratorDemoActivity.class);
