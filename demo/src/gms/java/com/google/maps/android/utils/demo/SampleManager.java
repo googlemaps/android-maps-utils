0adae9835f2b73f96bdc8d85433c2053fe2c34c0
Enrique López Mañas <eenriquelopez@gmail.com>
2022-11-27 17:11:40 +0100

    chore(dependencies): general update of dependencies

---
+++ demo/src/gms/java/com/google/maps/android/utils/demo/SampleManager.java
@@ -0,0 +1,16 @@
+package com.google.maps.android.utils.demo;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.maps.android.collections.GroundOverlayManager;
+
+
+import androidx.annotation.NonNull;
+
+public class SampleManager extends GroundOverlayManager implements GoogleMap.OnGroundOverlayClickListener {
+
+
+    public SampleManager(@NonNull GoogleMap map) {
+        super(map);
+    }
+}
