32924139b1ab202b84344135361351a2b9d1e0fa
Enrique López Mañas <eenriquelopez@gmail.com>
2025-03-11 15:46:13 +0100

    fix: fix for ConcurrentModificationException

--- library/src/main/java/com/google/maps/android/clustering/view/ClusterRendererMultipleItems.java
+++ library/src/main/java/com/google/maps/android/clustering/view/ClusterRendererMultipleItems.java
@@ -64,7 +64,6 @@ import com.google.maps.android.ui.SquareTextView;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
