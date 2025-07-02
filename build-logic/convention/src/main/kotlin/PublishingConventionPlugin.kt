6f6a82465b5b3ec2fbd5edfdfb340d5f3ea00ffa
Enrique López Mañas <eenriquelopez@gmail.com>
2025-07-02 12:20:43 +0200

    chore: migrated new Maven Central URL

--- build-logic/convention/src/main/kotlin/PublishingConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/PublishingConventionPlugin.kt
@@ -111,9 +111,9 @@ class PublishingConventionPlugin : Plugin<Project> {
             repositories {
                 maven {
                     val releasesRepoUrl =
-                        uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                        uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
                     val snapshotsRepoUrl =
-                        uri("https://oss.sonatype.org/content/repositories/snapshots/")
+                        uri("https://central.sonatype.com/repository/maven-snapshots/")
                     url = if (project.version.toString()
                             .endsWith("SNAPSHOT")
                     ) snapshotsRepoUrl else releasesRepoUrl
