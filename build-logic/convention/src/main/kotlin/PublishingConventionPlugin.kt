74a3d01e5245c0a05711388186e76c971c0b44f6
Enrique López-Mañas <eenriquelopez@gmail.com>
2025-07-17 17:44:08 +0200

    chore: migrating to vanniktech plugin (#1547)
    * chore: migrating to vanniktech plugin
    
    * chore: removed old properties

--- build-logic/convention/src/main/kotlin/PublishingConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/PublishingConventionPlugin.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Google LLC
+ * Copyright 2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,40 +15,34 @@
  */
 
 // buildSrc/src/main/kotlin/PublishingConventionPlugin.kt
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.publish.PublishingExtension
-import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.kotlin.dsl.*
 import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
 import org.gradle.api.tasks.testing.Test
 import org.gradle.testing.jacoco.plugins.JacocoTaskExtension
-import org.gradle.plugins.signing.SigningExtension
-import org.gradle.api.publish.maven.*
 
 class PublishingConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.run {
-
             applyPlugins()
             configureJacoco()
-            configurePublishing()
-            configureSigning()
+            configureVanniktechPublishing()
         }
     }
 
     private fun Project.applyPlugins() {
         apply(plugin = "com.android.library")
         apply(plugin = "com.mxalbert.gradle.jacoco-android")
-        apply(plugin = "maven-publish")
         apply(plugin = "org.jetbrains.dokka")
-        apply(plugin = "signing")
+        apply(plugin = "com.vanniktech.maven.publish")
     }
 
     private fun Project.configureJacoco() {
         configure<JacocoPluginExtension> {
             toolVersion = "0.8.7"
-
         }
 
         tasks.withType<Test>().configureEach {
@@ -59,76 +53,46 @@ class PublishingConventionPlugin : Plugin<Project> {
         }
     }
 
-    private fun Project.configurePublishing() {
-        extensions.configure<com.android.build.gradle.LibraryExtension> {
-            publishing {
-                singleVariant("release") {
-                    withSourcesJar()
-                    withJavadocJar()
-                }
-            }
-        }
-        extensions.configure<PublishingExtension> {
-            publications {
-                create<MavenPublication>("aar") {
-                    artifactId = if (project.name == "library") {
-                        "android-maps-utils"
-                    } else {
-                        null
-                    }
+    private fun Project.configureVanniktechPublishing() {
+        extensions.configure<MavenPublishBaseExtension> {
+            configure(
+                AndroidSingleVariantLibrary(
+                    variant = "release",
+                    sourcesJar = true,
+                    publishJavadocJar = true
+                )
+            )
 
-                    afterEvaluate {
-                        from(components["release"])
-                    }
-                    pom {
-                        name.set(project.name)
-                        description.set("Handy extensions to the Google Maps Android API.")
-                        url.set("https://github.com/googlemaps/android-maps-utils")
-                        scm {
-                            connection.set("scm:git@github.com:googlemaps/android-maps-utils.git")
-                            developerConnection.set("scm:git@github.com:googlemaps/android-maps-utils.git")
-                            url.set("scm:git@github.com:googlemaps/android-maps-utils.git")
-                        }
-                        licenses {
-                            license {
-                                name.set("The Apache Software License, Version 2.0")
-                                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                                distribution.set("repo")
-                            }
-                        }
-                        organization {
-                            name.set("Google Inc")
-                            url.set("http://developers.google.com/maps")
-                        }
-                        developers {
-                            developer {
-                                name.set("Google Inc.")
-                            }
-                        }
-                    }
-                }
-            }
-            repositories {
-                maven {
-                    val releasesRepoUrl =
-                        uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
-                    val snapshotsRepoUrl =
-                        uri("https://central.sonatype.com/repository/maven-snapshots/")
-                    url = if (project.version.toString()
-                            .endsWith("SNAPSHOT")
-                    ) snapshotsRepoUrl else releasesRepoUrl
-                    credentials {
-                        username = project.findProperty("sonatypeToken") as String?
-                        password = project.findProperty("sonatypeTokenPassword") as String?
-                    }
-                }
-            }
-        }
-    }
+            publishToMavenCentral()
+            signAllPublications()
 
-    private fun Project.configureSigning() {
-        configure<SigningExtension> {
-            sign(extensions.getByType<PublishingExtension>().publications["aar"])
+            pom {
+                name.set(project.name)
+                description.set("Handy extensions to the Google Maps Android API.")
+                url.set("https://github.com/googlemaps/android-maps-utils")
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                scm {
+                    connection.set("scm:git@github.com:googlemaps/android-maps-utils.git")
+                    developerConnection.set("scm:git@github.com:googlemaps/android-maps-utils.git")
+                    url.set("https://github.com/googlemaps/android-maps-utils")
+                }
+                developers {
+                    developer {
+                        id.set("google")
+                        name.set("Google Inc.")
+                    }
+                }
+                organization {
+                    name.set("Google Inc")
+                    url.set("http://developers.google.com/maps")
+                }
+            }
         }
     }
 }
