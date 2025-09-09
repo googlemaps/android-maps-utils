4ab85c2111e3146cc29524153f6ab48e22ddbe11
dkhawk <107309+dkhawk@users.noreply.github.com>
2025-09-09 15:53:13 -0600

    feat(demo): Enable edge-to-edge for StreetView demo activities
    This commit refactors the `StreetViewDemoActivity` in both its Kotlin and Java versions to support edge-to-edge display.
    
    The key changes include:
    - Changing the base class from `Activity` to `AppCompatActivity`.
    - Using `WindowCompat.setDecorFitsSystemWindows` to allow the app to draw behind the system bars.
    - Adding an `OnApplyWindowInsetsListener` to the root view to apply padding for the system bars, preventing content from being obscured.
    - Updating the `street_view_demo.xml` layout with an ID for the root view to facilitate the insets listener.
    - Updating the copyright year.

--- demo/src/main/java/com/google/maps/android/utils/demo/StreetViewDemoActivity.kt
+++ demo/src/main/java/com/google/maps/android/utils/demo/StreetViewDemoActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Google LLC
+ * Copyright 2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,35 +17,61 @@
 package com.google.maps.android.utils.demo
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.StreetViewUtils
-import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class StreetViewDemoActivity : Activity() {
+/**
+ * An activity that demonstrates how to use the [StreetViewUtils] to check for Street View
+ * availability at different locations.
+ *
+ * This activity performs the following actions:
+ * 1. Sets up the layout to fit system windows.
+ * 2. Checks if a valid Google Maps API key is present.
+ * 3. Launches a coroutine to fetch Street View data for two predefined locations.
+ * 4. Displays the results of the Street View data fetch on the screen.
+ */
+class StreetViewDemoActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
-    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Make the activity content fit behind the system bars.
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.street_view_demo)
 
+        // Apply window insets to the main view to avoid content overlapping with system bars.
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Check for a valid Maps API key before proceeding.
         if (!hasMapsApiKey(this)) {
             Toast.makeText(this, R.string.bad_maps_api_key, Toast.LENGTH_LONG).show()
             finish()
+            return // Return early to prevent further execution
         }
 
-        GlobalScope.launch(Dispatchers.Main) {
+        // Launch a coroutine in the Main dispatcher to fetch Street View data and update the UI.
+        lifecycleScope.launch(Dispatchers.Main) {
+            // Fetch Street View data for the first location (which is expected to be supported).
             val response1 =
                 StreetViewUtils.fetchStreetViewData(LatLng(48.1425918, 11.5386121), BuildConfig.MAPS_API_KEY)
+            // Fetch Street View data for the second location (which is expected to be unsupported).
             val response2 = StreetViewUtils.fetchStreetViewData(LatLng(8.1425918, 11.5386121), BuildConfig.MAPS_API_KEY)
 
+            // Update the UI with the results.
             findViewById<TextView>(R.id.textViewFirstLocation).text = "Location 1 is supported in StreetView: $response1"
             findViewById<TextView>(R.id.textViewSecondLocation).text = "Location 2 is  supported in StreetView: $response2"
         }
--- demo/src/main/java/com/google/maps/android/utils/demo/StreetViewDemoActivityJava.java
+++ demo/src/main/java/com/google/maps/android/utils/demo/StreetViewDemoActivityJava.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,75 +17,97 @@
 package com.google.maps.android.utils.demo;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.Status;
-import com.google.maps.android.utils.demo.BuildConfig;
-import com.google.maps.android.utils.demo.R;
 
 /**
- * This activity demonstrates how to use the Street View utility in Java.
+ * An activity that demonstrates how to use the Street View utility in Java to check for Street View
+ * availability at different locations.
+ *
+ * This activity performs the following actions:
+ * 1. Sets up the layout to fit system windows.
+ * 2. Checks if a valid Google Maps API key is present.
+ * 3. Fetches Street View data for two predefined locations using asynchronous callbacks.
+ * 4. Displays the results of the Street View data fetch on the screen.
  */
-public class StreetViewDemoActivityJava extends Activity {
+public class StreetViewDemoActivityJava extends AppCompatActivity {
 
+    /**
+     * Called when the activity is first created.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  Otherwise it is null.
+     */
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Make the activity content fit behind the system bars.
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         setContentView(R.layout.street_view_demo);
 
+        // Apply window insets to the main view to avoid content overlapping with system bars.
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insetsCompat) -> {
+            Insets insets = insetsCompat.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(insets.left, insets.top, insets.right, insets.bottom);
+            return insetsCompat;
+        });
+
+        // Check for a valid Maps API key before proceeding.
         if (!ApiKeyValidator.hasMapsApiKey(this)) {
             Toast.makeText(this, R.string.bad_maps_api_key, Toast.LENGTH_LONG).show();
             finish();
-            return;
+            return; // Return early to prevent further execution
         }
 
-        // Fetches Street View data for the first location.
+        // Fetches Street View data for the first location (expected to be supported).
         StreetViewHelper.fetchStreetViewData(
-            new LatLng(48.1425918, 11.5386121),
-            BuildConfig.MAPS_API_KEY,
-            new StreetViewHelper.StreetViewCallback() {
-                @Override
-                public void onStreetViewResult(Status status) {
-                    // Updates the UI with the result.
-                    runOnUiThread(() -> {
-                        ((TextView) findViewById(R.id.textViewFirstLocation)).setText("Location 1 is supported in StreetView: " + status);
-                    });
-                }
+                new LatLng(48.1425918, 11.5386121),
+                BuildConfig.MAPS_API_KEY, new StreetViewHelper.StreetViewCallback() {
+                    @Override
+                    public void onStreetViewResult(@NonNull Status status) {
+                        // Updates the UI with the result on the UI thread.
+                        runOnUiThread(() -> {
+                            ((TextView) findViewById(R.id.textViewFirstLocation)).setText("Location 1 is supported in StreetView: " + status);
+                        });
+                    }
 
-                @Override
-                public void onStreetViewError(Exception e) {
-                    // Handles the error.
-                    e.printStackTrace();
-                    Toast.makeText(StreetViewDemoActivityJava.this, "Error fetching Street View data: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
+                    @Override
+                    public void onStreetViewError(@NonNull Exception e) {
+                        // Handles the error by printing stack trace and showing a toast.
+                        e.printStackTrace();
+                        Toast.makeText(StreetViewDemoActivityJava.this, "Error fetching Street View data: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Fetches Street View data for the second location (expected to be unsupported).
+        StreetViewHelper.fetchStreetViewData(new LatLng(8.1425918, 11.5386121), BuildConfig.MAPS_API_KEY, new StreetViewHelper.StreetViewCallback() {
+            @Override
+            public void onStreetViewResult(@NonNull Status status) {
+                // Updates the UI with the result on the UI thread.
+                runOnUiThread(() -> {
+                    ((TextView) findViewById(R.id.textViewSecondLocation)).setText("Location 2 is  supported in StreetView: " + status);
+                });
             }
-        );
 
-        // Fetches Street View data for the second location.
-        StreetViewHelper.fetchStreetViewData(
-            new LatLng(8.1425918, 11.5386121),
-            BuildConfig.MAPS_API_KEY,
-            new StreetViewHelper.StreetViewCallback() {
-                @Override
-                public void onStreetViewResult(Status status) {
-                    // Updates the UI with the result.
-                    runOnUiThread(() -> {
-                        ((TextView) findViewById(R.id.textViewSecondLocation)).setText("Location 2 is  supported in StreetView: " + status);
-                    });
-                }
-
-                @Override
-                public void onStreetViewError(Exception e) {
-                    // Handles the error.
-                    e.printStackTrace();
-                    Toast.makeText(StreetViewDemoActivityJava.this, "Error fetching Street View data: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
+            @Override
+            public void onStreetViewError(@NonNull Exception e) {
+                // Handles the error by printing stack trace and showing a toast.
+                e.printStackTrace();
+                Toast.makeText(StreetViewDemoActivityJava.this, "Error fetching Street View data: " + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
-        );
+        });
     }
 }
