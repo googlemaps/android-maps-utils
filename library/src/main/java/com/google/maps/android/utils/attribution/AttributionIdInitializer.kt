e91cc59ed531385c514b2eb186c9e519fdefa95b
dkhawk <107309+dkhawk@users.noreply.github.com>
2026-01-22 17:17:00 -0700

    style: fix long kdoc lines in AttributionIdInitializer

--- library/src/main/java/com/google/maps/android/utils/attribution/AttributionIdInitializer.kt
+++ library/src/main/java/com/google/maps/android/utils/attribution/AttributionIdInitializer.kt
@@ -22,7 +22,8 @@ import com.google.android.gms.maps.MapsApiSettings
 import com.google.maps.android.utils.meta.AttributionId
 
 /**
- * Adds a usage attribution ID to the initializer, which helps Google understand which libraries and samples are helpful to developers, such as usage of this library.
+ * Adds a usage attribution ID to the initializer, which helps Google understand which libraries
+ * and samples are helpful to developers, such as usage of this library.
  * To opt out of sending the usage attribution ID, please remove this initializer from your manifest.
  */
 internal class AttributionIdInitializer : Initializer<Unit> {
