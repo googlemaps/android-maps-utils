91e19089aaf84af89274bd991ffd87574e252b00
Enrique López Mañas <eenriquelopez@gmail.com>
2026-01-23 13:26:27 +0100

    build: release simple

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,5 +37,7 @@ tasks.register<Delete>("clean") {
 
 allprojects {
     group = "com.google.maps.android"
-    version = project.property("version") as String
+    // {x-release-please-start-version}
+    version = "3.20.1"
+    // {x-release-please-end}
 }
