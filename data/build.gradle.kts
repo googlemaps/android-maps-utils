d33ddc32baa52534ea4ed737d68ab4a54cadfdec
Enrique López Mañas <eenriquelopez@gmail.com>
2026-02-26 13:58:51 +0100

    docs: add license header as a new year

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 /**
- * Copyright 2024 Google LLC
+ * Copyright 2026Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
