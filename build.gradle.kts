1b3e1747751f29c1d019699e08ae3a12031bda8e
Dale Hawkins <107309+dkhawk@users.noreply.github.com>
2026-08-28 17:24:42 -0600

    chore: release v6.0.0-rc02

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ tasks.register<Exec>("installAndLaunch") {
 allprojects {
     group = "com.google.maps.android"
     // {x-release-please-start-version}
-    version = "6.0.0-rc01"
+    version = "6.0.0-rc02"
     // {x-release-please-end}
 
     plugins.withId("java") {
